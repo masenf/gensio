f8d49c36096528847580e04c6015af05873aa035
Masen Furer <m_github@0x26.net>
2022-09-11 19:25:28 -0700

    setup.py: add PyPI-compatible setup script
    enable build and deploy of python bindings from PyPI

---
+++ setup.py
@@ -0,0 +1,120 @@
+"""
+PyPI / manylinux compatible sdist + bdist_wheel package.
+
+Buildable without additional packages in the
+quay.io/pypa/manylinux2014_x86_64 docker image
+"""
+from contextlib import contextmanager
+import errno
+import os
+from pathlib import Path
+import sysconfig
+
+from setuptools import setup, Extension, find_packages
+from distutils.command import build_ext as _build_ext
+from distutils.dir_util import mkpath
+from distutils.file_util import copy_file
+from distutils.spawn import spawn
+
+
+here = Path( __file__ ).parent.absolute()
+gensio_lib_output = here / "lib" / ".libs"
+gensio_swig_output = here / "swig" / "python" / ".libs"
+readme = long_description = (here / "README.rst").read_text()
+skip_shared_object_link_check_patch = here / "swig" / "python" / "patch--m4--ax_python_devel.m4.patch"
+
+
+class build_ext(_build_ext.build_ext):
+    """
+    Build extension using GNU Autotools.
+
+    Re-use the package's existing build system, while bundling compiled
+    libraries into the python package.
+    """
+    def patch_rpath(self, lib):
+        copy_file(str(lib), self.build_lib)
+        # re-home the runpath to allow --user installation from sdist
+        spawn(
+            [
+                "patchelf",
+                "--set-rpath",
+                sysconfig.get_path("platlib"),
+                str(Path(self.build_lib) / lib.name),
+            ]
+        )
+
+    @contextmanager
+    def patch_autoconf(self):
+        """
+        skip autoconf check that it can link against libpython.
+
+        python extensions don't need to link against libpython and are not allowed
+        to in manylinux, so remove this check.
+        """
+        spawn(
+            [
+                "patch",
+                "--forward",
+                str(here / "m4" / "ax_python_devel.m4"),
+                str(skip_shared_object_link_check_patch),
+            ]
+        )
+        try:
+            yield
+        finally:
+            spawn(
+                [
+                    "patch",
+                    "--reverse",
+                    str(here / "m4" / "ax_python_devel.m4"),
+                    str(skip_shared_object_link_check_patch),
+                ]
+            )
+
+    def run(self):
+        if not self.extensions:
+            return
+        os.environ["PYTHON_VERSION"] = sysconfig.get_python_version()
+        with self.patch_autoconf():
+            configure = here / "configure"
+            if not configure.exists():
+                spawn([str(here / "reconf")])
+            spawn(
+                [
+                    str(configure),
+                    "--with-python",
+                    # normally this would be automatic... but explicitly override it
+                    # to avoid the `-lpythonX.Y` flag, which isn't supported by manylinux
+                    "PYTHON_LIBS={}".format(sysconfig.get_config_var("LIBDIR")),
+                ],
+            )
+            spawn(["make", "-C", str(here)])
+        mkpath(self.build_lib)
+        for ext in self.extensions:
+            for lib_dir in ext.depends or []:
+                for lib in lib_dir.glob("*.so*"):
+                    # TODO: avoid copying symlinks
+                    self.patch_rpath(lib)
+
+
+setup(
+    name='gensio',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    url='https://github.com/cminyard/gensio',
+    author='Corey Minyard',
+    author_email='cminyard@mvista.com>',
+    description='a framework for giving a consistent view of various stream (and packet) I/O types',
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    py_modules=["gensio"],
+    package_dir={'': "swig/python"},
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
+        Extension(
+            name="gensio",
+            sources=[],  # overridden in build_ext
+            depends=[gensio_swig_output, gensio_lib_output],
+        ),
+    ],
+)
